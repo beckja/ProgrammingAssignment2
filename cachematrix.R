16cec8cfcb895220601519f560b8bc9aac6789bb
Jeff Beck <beckja@alum.lehigh.edu>
2017-04-21 08:06:51 -0700

    Programming Assignment 2 initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Collection of functions to support caching of matrix inverses
 
-## Write a short comment describing this function
+## Usage:
+##   m <- matrix(rnorm(9), 3, 3)
+##   M <- makeCacheMatrix(m)
+##   Minv <- cacheSolve(M)
 
-makeCacheMatrix <- function(x = matrix()) {
+## Verification:
+##   Minv <- cacheSolve(M)  # should see "getting cached data" message
+##   Minv %*% M$get()  # should return 3 x 3 identity matrix (within tolerances)
+##   M$get() %*% Minv  # should return 3 x 3 identity matrix (within tolerances)
 
+## Creates a cache object that stores a matrix and its inverse
+
+makeCacheMatrix <- function(my_x = matrix()) {
+  my_xinv <- NULL
+  set <- function(x) {
+    my_x <<- x
+    my_xinv <<- NULL
+  }
+  get <- function() my_x
+  setinverse <- function(xinv) my_xinv <<- xinv
+  getinverse <- function() my_xinv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes a matrix inverse, pulling from cache if previously computed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getinverse()
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  data <- x$get()
+  xinv <- solve(data, ...)
+  x$setinverse(xinv)
+  xinv
 }
